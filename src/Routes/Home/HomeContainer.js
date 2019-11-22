e075487178792878f5724b740ddf68f10e4e35d2
skwzz <kidol9108@gmail.com>
2019-11-22 16:35:15 +0900

    HomeContinaer, HomePresenter

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HomePresenter from "./HomePresenter";
+import { movieApi } from "api";
 
 export default class extends React.Component {
   state = {
@@ -10,8 +11,36 @@ export default class extends React.Component {
     loading: true
   };
 
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: nowPlaying }
+      } = await movieApi.nowPlaying();
+      const {
+        data: { results: upComing }
+      } = await movieApi.upComing();
+      const {
+        data: { results: popular }
+      } = await movieApi.popular();
+      this.setState({
+        nowPlaying,
+        upComing,
+        popular
+      });
+    } catch {
+      this.setState({
+        error: "Sorry! Can't find movies information."
+      });
+    } finally {
+      this.setState({
+        loading: false
+      });
+    }
+  }
+
   render() {
     const { nowPlaying, upComing, popular, error, loading } = this.state;
+    console.log(this.state);
     return (
       <HomePresenter
         nowPlaying={nowPlaying}
