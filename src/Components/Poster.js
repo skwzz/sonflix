924e4165fbdee1a0d984f9e5f97c8903b756fce0
skwzz <kidol9108@gmail.com>
2019-11-28 13:32:13 +0900

    Poster Component

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -2,18 +2,55 @@ import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-const Container = styled.div``;
-const ImageContainer = styled.div``;
-const Image = styled.div``;
-const Rating = styled.span``;
-const Title = styled.span``;
-const Year = styled.span``;
+const Container = styled.div`
+  font-size: 12px;
+`;
+const Image = styled.div`
+  background-image: url(${props => props.bgUrl});
+  height: 180px;
+  background-size: cover;
+  border-radius: 4px;
+  background-position: center center;
+  transition: opacity 0.2s linear;
+`;
+const Rating = styled.span`
+  bottom: 5px;
+  right: 5px;
+  position: absolute;
+  opacity: 0;
+  transition: opacity 0.2s linear;
+`;
+const ImageContainer = styled.div`
+  margin-bottom: 5px;
+  position: relative;
+  &:hover {
+    ${Image} {
+      opacity: 0.3;
+    }
+    ${Rating} {
+      opacity: 1;
+    }
+  }
+`;
+const Title = styled.span`
+  display: block;
+  margin-bottom: 3px;
+`;
+const Year = styled.span`
+  color: rgba(255, 255, 255, 0.7);
+`;
 
 const Poster = ({ id, imageUrl, title, rating, year, isMovie = false }) => (
   <Link to={isMovie ? `/movie/${id}` : `/tv/${id}`}>
     <Container>
       <ImageContainer>
-        <Image bgUrl={imageUrl} />
+        <Image
+          bgUrl={
+            imageUrl
+              ? `https://image.tmdb.org/t/p/w300${imageUrl}`
+              : require("../Assets/no_poster_available.png")
+          }
+        />
         <Rating>
           <span role="img" aria-label="rating">
             👍
@@ -21,7 +58,9 @@ const Poster = ({ id, imageUrl, title, rating, year, isMovie = false }) => (
           {rating}/10
         </Rating>
       </ImageContainer>
-      <Title>{title}</Title>
+      <Title>
+        {title.length > 20 ? `${title.substring(0, 20)}...` : title}
+      </Title>
       <Year>{year}</Year>
     </Container>
   </Link>
--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -1,8 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Loader from "Components/Loader";
 
-const DetailPresenter = ({ result, loading, error }) => null;
+const Container = styled.div`
+  height: calc(100vh - 50px);
+  width: 100%;
+  position: relative;
+`;
+
+const BackDrop = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url(${props => props.bgImage});
+  background-position: center center;
+  background-size: cover;
+`;
+
+const DetailPresenter = ({ result, loading, error }) =>
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      <BackDrop
+        bgImage={`http://image.tmdb.com/t/p/original${result.backdrop_path}`}
+      />
+    </Container>
+  );
 DetailPresenter.propTypes = {
   result: PropTypes.object,
   loading: PropTypes.bool.isRequired,
