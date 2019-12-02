571468ae3d2cfbb74de73c7cc53064bf8ecea500
skwzz <kidol9108@gmail.com>
2019-12-02 17:47:06 +0900

    Updates

---
+++ precache-manifest.911dddcc6c1f3b1fa21d082f0603ef41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abd09b38b11fba5263dbecd220843539",
+    "url": "/sonflix/index.html"
+  },
+  {
+    "revision": "35c7d869f1859b63e0ef",
+    "url": "/sonflix/static/js/2.49dad730.chunk.js"
+  },
+  {
+    "revision": "f8d04342eaf097d0e1f5",
+    "url": "/sonflix/static/js/main.42ced844.chunk.js"
+  },
+  {
+    "revision": "0ef276a5816685fc3b54",
+    "url": "/sonflix/static/js/runtime-main.b037b435.js"
+  },
+  {
+    "revision": "d7ef1547b30065be8e3d74e898848498",
+    "url": "/sonflix/static/media/no_poster_available.d7ef1547.png"
+  }
+]);
